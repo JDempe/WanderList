7753c2682e6990f3b2db776a2d1a0878a0963adc
JDempe <123279032+JDempe@users.noreply.github.com>
2023-05-12 10:01:52 -0400

    Added DB name and fixed project format

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const routes = require('./routes');
+const routes = require('./controllers');
 const hbs = exphbs.create({});
 
 const sequelize = require('./config/connection');
