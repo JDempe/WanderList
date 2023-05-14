7cdd9f661c5056bf4d4d5c3b6e3fc5e9d7d289ce
bandhavi <bandhavi.bendi@coupa.com>
2023-05-14 07:44:00 -0700

    controllers for avatar

--- controllers/apiRoutes/index.js
+++ controllers/apiRoutes/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const avatarRoutes = require('./avatarRoutes');
 
 router.use('/user', userRoutes);
+router.use('/user', avatarRoutes );
+
 
 
 module.exports = router;
