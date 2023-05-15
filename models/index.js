37ae3da87c9891236ebd6f94bd4cafd230fc2027
JDempe <123279032+JDempe@users.noreply.github.com>
2023-05-15 10:02:32 -0400

    Fixed Pin model path issue and added bulkCreate

--- models/index.js
+++ models/index.js
@@ -1,12 +1,12 @@
 // Purpose: Export all models
 const Destinations = require("./Destinations");
 const Avatar = require("./Avatars");
-const Pin = require("./Pins");
+const Pins = require("./Pins");
 const User = require('./User');
 
 module.exports = {
   Destinations,
   User,
   Avatar,
-  Pin,
+  Pins,
 };
