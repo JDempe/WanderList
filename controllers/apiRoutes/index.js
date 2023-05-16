f3b587ac99caa3fe03d399fbba9e8feec9979e87
bandhavi <bandhavi.b12@gmail.com>
2023-05-15 19:38:15 -0700

    git test

--- controllers/apiRoutes/index.js
+++ controllers/apiRoutes/index.js
@@ -5,7 +5,7 @@ const destinationRoutes = require('./pinRoutes');
 
 router.use('/user', userRoutes);
 router.use('/user', avatarRoutes );
-router.use('/user', destinationRoutes ); 
+router.use('/user', destinationRoutes);
 
 
 
