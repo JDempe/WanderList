a9be4847f2cffb68833ad0d3f7c3ead8f3b86970
Seer <kmkevins97@gmail.com>
2023-05-16 11:03:52 -0700

    Testing editing functionality of card, now switches between editing state or read

--- public/js/user-page.js
+++ public/js/user-page.js
@@ -1,14 +1,14 @@
 // Search function Javascript / Searches for date, title, and text
-$(document).ready(function() {
+const pinSearch = function() {
     $('#search-input').on('input', function() {
-      var searchText = $(this).val().toLowerCase();
-      var noResultsContainer = $('.no-results-container');
+      const searchText = $(this).val().toLowerCase();
+      const noResultsContainer = $('.no-results-container');
   
       $('.pin').each(function() {
-        var pin = $(this);
-        var cardTitle = pin.find('.card-title').text().toLowerCase();
-        var cardText = pin.find('.card-text').text().toLowerCase();
-        var timestamp = pin.find('.timestamp').text().toLowerCase();
+        const pin = $(this);
+        const cardTitle = pin.find('.card-title').text().toLowerCase();
+        const cardText = pin.find('.card-text').text().toLowerCase();
+        const timestamp = pin.find('.timestamp').text().toLowerCase();
   
         if (
           cardTitle.includes(searchText) ||
@@ -25,50 +25,95 @@ $(document).ready(function() {
         }
       });
     });
-});
+};
 
 // Card Expansion and Collapse Javascript 
-$(document).ready(function() {
+const pinTransition = function() {
     $(".card").each(function() {
-        var card = $(this);
-        var cardHeight = card.outerHeight();
-        if (cardHeight > 200) {
-            card.find(".truncated-text").addClass("enable-truncation");
-            card.find(".expand-button").show();
-        }
-    });
-
-    $(".expand-button").click(function() {
-        var card = $(this).closest(".card");
-        card.removeClass("collapsing");
-        card.addClass("expanded");
-        card.find(".truncated-text").css("max-height", card.find(".truncated-text")[0].scrollHeight + "px");
-        $(this).hide();
-        card.find(".collapse-button").show();
-    });
-
-    $(".collapse-button").click(function() {
-        var card = $(this).closest(".card");
-        card.removeClass("expanded");
-        card.addClass("collapsing");
-        card.find(".truncated-text").css("max-height", "200px");
-        $(this).hide();
+      const card = $(this);
+      const cardHeight = card.outerHeight();
+      if (cardHeight > 200) {
+        card.find(".truncated-text").addClass("enable-truncation");
         card.find(".expand-button").show();
+      }
     });
-});
+  
+    $(".expand-button").click(function() {
+      const card = $(this).closest(".card");
+      card.removeClass("collapsing");
+      card.addClass("expanded");
+      card.find(".truncated-text").css("max-height", card.find(".truncated-text")[0].scrollHeight + "px");
+      $(this).hide();
+      card.find(".collapse-button").show();
+    });
+  
+    $(".collapse-button").click(function() {
+      const card = $(this).closest(".card");
+      card.removeClass("expanded");
+      card.addClass("collapsing");
+      card.find(".truncated-text").css("max-height", "200px");
+      $(this).hide();
+      card.find(".expand-button").show();
+    });
+};
+
+// Hide textareas by default
+$('.title-textarea').hide();
+$('.card-textarea').hide();
+
+// Function to handle click outside of .card-container
+function handleClickOutside(event) {
+  const cardContainers = $('.card-container');
+
+  cardContainers.each(function() {
+    const cardContainer = $(this);
+
+    const titleTextarea = cardContainer.find('.title-textarea');
+    const cardTextarea = cardContainer.find('.card-textarea');
+    const displayText = cardContainer.find('.display-text');
+
+    if (
+      !cardContainer.is(event.target) &&
+      cardContainer.has(event.target).length === 0
+    ) {
+      // Hide textareas and show display elements
+      titleTextarea.hide();
+      cardTextarea.hide();
+      displayText.show();
+    }
+  });
+}
 
 // Allows editing of card when the pencil icon is clicked
 $('.card-icon-section .bi-pencil').click(function() {
-    // Disables editing on all cards
-    $('.card-title').prop('readonly', true);
-    $('.card-text').prop('readonly', true);
-  
-    // Enables editing on the clicked card
-    let cardTitleInput = $(this).closest('.card-top-section').find('.card-title');
-    let cardTextInput = $(this).closest('.card-body').find('.card-text');
-  
-    cardTitleInput.prop('readonly', false);
-    cardTextInput.prop('readonly', false);
-  
-    cardTitleInput.focus();
-  });
+  // Disable editing on all cards
+  $('.title-textarea').prop('readonly', true);
+  $('.card-textarea').prop('readonly', true);
+
+  // Enables editing on the clicked card
+  const cardTitleInput = $(this).closest('.card-top-section').find('.card-title');
+  const cardTextInput = $(this).closest('.card-body').find('.card-text');
+  const displayText = $(this).closest('.card-body').find('.display-text');
+
+  // Hide display elements and show textareas
+  cardTitleInput.hide();
+  cardTextInput.hide();
+  displayText.hide();
+  cardTitleInput.siblings('.title-textarea').show();
+  cardTextInput.siblings('.card-textarea').show();
+
+  // Enable editing
+  cardTitleInput.prop('readonly', false);
+  cardTextInput.prop('readonly', false);
+
+  // Focus on the title textarea
+  cardTitleInput.siblings('.title-textarea').focus();
+});
+
+// Bind click event to the document
+$(document).on('click', handleClickOutside);
+    
+$(document).ready(function() {
+    pinSearch();
+    pinTransition();
+});
