561d5149975de4f6ccb83b9c56b80c6cbf20c0e0
JDempe <123279032+JDempe@users.noreply.github.com>
2023-05-17 01:54:17 -0400

    Added more avatars and simplebars for cleaner modal scrollbar

--- seeds/index.js
+++ seeds/index.js
@@ -1,27 +1,31 @@
 const sequelize = require("../config/connection");
-const { loremIpsum } = require("lorem-ipsum");
 const { User, Pins, Avatars } = require("../models");
 
 const userSeedData = require("./userSeedData.json");
 const pinSeedData = require("./pinSeedData.json");
-const seedAvatars = require('./seedAvatars');
-console.log(seedAvatars); 
+const avatarSeedData = require("./avatarSeedData.json");
+
 const seedDb = async () => {
   await sequelize.sync({ force: true });
-  console.log('\n----- database synced -----\n');
+  console.log("\n----- database synced -----\n");
 
   await User.bulkCreate(userSeedData, {
     individualHooks: true,
     returning: true,
   });
+  console.log("'\n ----- Users seeded -----\n");
 
   await Pins.bulkCreate(pinSeedData, {
     individualHooks: true,
     returning: true,
   });
+  console.log("'\n ----- Pins seeded -----\n");
 
-  await seedAvatars();
-  
+  await Avatars.bulkCreate(avatarSeedData, {
+    individualHooks: true,
+    returning: true,
+  });
+
   console.log("'\n ----- Avatars seeded -----\n");
   process.exit(0);
 };
