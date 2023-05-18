785f23fddcd1355b5d97079ab63f1e2c68ac7c49
Jen Ryt <jenyjlin@gmail.com>
2023-05-18 14:52:31 -0700

    update pin seed data

--- controllers/apiRoutes/pinRoutes.js
+++ controllers/apiRoutes/pinRoutes.js
@@ -1,5 +1,6 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
+const router = require("express").Router();
 const { Pins } = require("../../models");
 
 // GET route to retrieve all pins
