2ca81410090273a804dbafd04331e31617150c53
Jen Ryt <jenyjlin@gmail.com>
2023-05-18 14:03:25 -0700

    updated pinRoutes

--- controllers/apiRoutes/index.js
+++ controllers/apiRoutes/index.js
@@ -1,12 +1,10 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const avatarRoutes = require('./avatarRoutes');
-const destinationRoutes = require('./pinRoutes');
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const avatarRoutes = require("./avatarRoutes");
+const pinRoutes = require("./pinRoutes");
 
-router.use('/user', userRoutes);
-router.use('/user', avatarRoutes );
-router.use('/user', destinationRoutes);
-
-
+router.use("/user", userRoutes);
+router.use("/", avatarRoutes);
+router.use("/", pinRoutes);
 
 module.exports = router;
--- controllers/apiRoutes/pinRoutes.js
+++ controllers/apiRoutes/pinRoutes.js
@@ -1,72 +1,85 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { Pins } = require('../../models');
+const { Pins } = require("../../models");
 
 // GET route to retrieve all pins
-router.get('/pins', async (req, res) => {
-    try {
-        const pins = await Pins.findAll();
-        res.status(200).json(pins);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+router.get("/pins", async (req, res) => {
+  try {
+    const pins = await Pins.findAll();
+    res.status(200).json(pins);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // GET route to retrieve a specific pin by ID
-router.get('/pins/:id', async (req, res) => {
-    try {
-        const pin = await Pins.findByPk(req.params.id);
-        if (pins) {
-            res.status(200).json(pins);
-        } else {
-            res.status(404).json({ error: 'Pin not found' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+router.get("/pins/:id", async (req, res) => {
+  try {
+    const pins = await Pins.findByPk(req.params.id);
+    if (pins) {
+      res.status(200).json(pins);
+    } else {
+      res.status(404).json({ error: "Pin not found" });
     }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // POST route to create a new pin
-router.post('/pins', async (req, res) => {
-    try {
-        const pins = await Pins.create(req.body);
-        res.status(201).json(pins);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+router.post("/pins", async (req, res) => {
+  try {
+    const newPins = await Pins.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(201).json(newPins);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 // PUT route to update a pin
-router.put('/pins/:id', async (req, res) => {
-    try {
-        const [updated] = await Pins.update(req.body, {
-            where: { id: req.params.id },
-        });
-        if (updated) {
-            const updatedPin = await Pins.findByPk(req.params.id);
-            res.status(200).json(updatedPin);
-        } else {
-            res.status(404).json({ error: 'Pin not found' });
-        }
-    } catch (error) {
-        res.status(400).json({ error: error.message });
+router.put("/pins/:id", async (req, res) => {
+  try {
+    const [updated] = await Pins.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (updated !== 0) {
+      const updatedPin = await Pins.findByPk(req.params.id);
+      res.status(200).json(updatedPin);
+    } else {
+      const existingPin = Pins.findByPk(req.params.id);
+      if (existingPin) {
+        res
+          .status(200)
+          .json({ message: "No update has been mapFinderOptions." });
+      } else {
+        res.status(404).json({ error: "Pin not found" });
+      }
     }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 // DELETE route to delete a Pin
-router.delete('/pins/:id', async (req, res) => {
-    try {
-        const deleted = await Pins.destroy({
-            where: { id: req.params.id },
-        });
-        if (deleted) {
-            res.status(204).json({ message: 'Pin deleted' });
-        } else {
-            res.status(404).json({ error: 'Pin not found' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+router.delete("/pins/:id", async (req, res) => {
+  try {
+    const pinData = await Pins.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+    if (pinData) {
+      res.status(204).json({ message: "Pin deleted" });
+    } else {
+      res.status(404).json({ error: "Pin not found" });
     }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = router;
