f7fc3d53d7cc9ae4b1c8d4922c1dae92e3bb4f6f
Seer <kmkevins97@gmail.com>
2023-05-18 19:53:15 -0700

    Established personal-page.handlebars and added related files

--- controllers/htmlRoutes/index.js
+++ controllers/htmlRoutes/index.js
@@ -18,6 +18,23 @@ router.get("/discover", async (req, res) => {
 });
 
 
+// GET discovery page
+router.get("/personal", async (req, res) => {
+  try {
+    //Serves the body of the page aka "discovery-page.hbs" to the container //aka "main.hbs"
+    // layout property not necessary since it is defaust, but included for clarity
+    res.render("personal-page", {
+      style: "./css/personal-page.css",
+      script: "./js/personal-page.js",
+      scriptSecond: "./js/search-pin.js",
+      partials: "personal-pin",
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 // GET user page
 router.get("/user/:id", async (req, res) => {
   try {
