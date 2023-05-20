545356328bdfc8a7b9c3a004eb04b63305422c37
Jen Ryt <jenyjlin@gmail.com>
2023-05-20 03:24:24 -0700

    1)user login redirecting to user home page; 2)potentially impelment the check box for pin card (need to be tested); 3)fixed typo for pinLocation; 4)increase pinSeedData from 3 pins to 9 pins; 5)change pinModel>>pinDescription type: DataTypes.TEXT

--- controllers/apiRoutes/userRoutes.js
+++ controllers/apiRoutes/userRoutes.js
@@ -1,175 +1,206 @@
-const router = require('express').Router();
-const bcrypt = require('bcrypt');
-const { User } = require('../../models');
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const { User } = require("../../models");
 
 // set up a route for users to sign up.
-router.post('/signup', async (req, res) => {
-    try {
-        // verify that there is no user in the database with the provided email
-        const userExists = await User.findOne({
-            where: {
-                email: req.body.email.toLowerCase().trim(),
-            }
-        });
-            
-        if (userExists) {
-            return res.json({ message: `The user with the provided email "${userExists.email}" already exists. Please try to log in.` });
-        }
-        
-        // verify that the provided username does not exist in the database.
-        const usernameExists = await User.findOne({
-            where: {
-                username: req.body.username.trim(),
-            }
-        })
-        
-        if (usernameExists) {
-            return res.json({ message: `The username "${usernameExists.username}" is already taken. Please choose a different username.` });
-        }
-        
-        // once all verification steps have been successfully completed, proceed to create the user in the database.
-        const result = await User.create({
-            username: req.body.username.trim(),
-            email: req.body.email.toLowerCase().trim(),
-            password: req.body.password.trim(),
-        });
+router.post("/signup", async (req, res) => {
+  try {
+    // verify that there is no user in the database with the provided email
+    const userExists = await User.findOne({
+      where: {
+        email: req.body.email.toLowerCase().trim(),
+      },
+    });
 
-        req.session.user_id = result.id;
-        req.session.logged_in = 1;
-
-        req.session.save();
-
-        res.status(200).json({ message: `Welcome aboard, ${result.username}! Enjoy your journey with us!` });
-        
-    } catch (error) {
-        res.status(500).json(error);
+    if (userExists) {
+      return res.json({
+        message: `The user with the provided email "${userExists.email}" already exists. Please try to log in.`,
+      });
     }
+
+    // verify that the provided username does not exist in the database.
+    const usernameExists = await User.findOne({
+      where: {
+        username: req.body.username.trim(),
+      },
+    });
+
+    if (usernameExists) {
+      return res.json({
+        message: `The username "${usernameExists.username}" is already taken. Please choose a different username.`,
+      });
+    }
+
+    // once all verification steps have been successfully completed, proceed to create the user in the database.
+    const result = await User.create({
+      username: req.body.username.trim(),
+      email: req.body.email.toLowerCase().trim(),
+      password: req.body.password.trim(),
+    });
+
+    req.session.user_id = result.id;
+    req.session.logged_in = 1;
+
+    req.session.save();
+
+    res.status(200).json({
+      message: `Welcome aboard, ${result.username}! Enjoy your journey with us!`,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // set up a route for users to log in.
-router.post('/login', async (req, res) => {
-    try {
-        // verify the existence of the user with the provided email in the database.
-        const userData = await User.findOne({
-            where: {
-                email: req.body.email.toLowerCase().trim(),
-            }
-        });
+router.post("/login", async (req, res) => {
+  try {
+    // verify the existence of the user with the provided email in the database.
+    const userData = await User.findOne({
+      where: {
+        email: req.body.email.toLowerCase().trim(),
+      },
+    });
 
-        if (!userData) {
-            return res.status(400).json({ message: 'Incorrect email or password, please try again' });
-        }
-        
-        // verify that the hashed password stored in the database matches the password provided by the user.
-        const validatePassword = bcrypt.compareSync(req.body.password, userData.password);
-        
-        if (!validatePassword) {
-            return res.status(400).json({ message: 'Incorrect email or password, please try again' });
-        }
-        
-        req.session.user_id = userData.id;
-        req.session.logged_in = 1;
-
-        req.session.save();
-        
-        return res.status(200).json({ message: 'You have been successfully logged in!' })
-        
-    } catch (error) {
-        res.status(500).json(error);
+    if (!userData) {
+      return res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
     }
+
+    // verify that the hashed password stored in the database matches the password provided by the user.
+    const validatePassword = bcrypt.compareSync(
+      req.body.password,
+      userData.password
+    );
+
+    if (!validatePassword) {
+      return res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+    }
+    const userId = userData.id;
+    console.log("!!!!!userData.id!!", userData.id);
+    req.session.user_id = userId;
+    req.session.logged_in = 1;
+
+    req.session.save();
+
+    return res
+      .status(200)
+      .json({ userId, message: "You have been successfully logged in!" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // route for user to logout
-router.post('/logout', async (req, res) => {
-    try {
-        if (req.session.logged_in) {
-            req.session.destroy( () => {
-                return res.status(200).end();
-            });
-        } else {
-            return res.status(404).end();
-        }
-    } catch (error) {
-        res.status(500).json(error);
+router.post("/logout", async (req, res) => {
+  try {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        return res.status(200).end();
+      });
+    } else {
+      return res.status(404).end();
     }
-})
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // set up a route for users to update information
 router.put("/editprofile/:id", async (req, res) => {
-    try {
-      // TODO verify that the user exists in the database.
-      const userExists = await User.findByPk(req.params.id);
-  
-      if (!userExists) {
-        return res.status(400).json({
-          message: `The user with the provided id "${req.params.id}" does not exist. Please try again.`,
-        });
-      }
-  
-      // proceed to update the user in the database.
-      const result = await User.update(
-        {
-          first_name: req.body.first_name,
-          last_name: req.body.last_name,
-          username: req.body.username,
-          about_me: req.body.about_me,
-          avatar_id: req.body.avatar_id,
-        },
-        {
-          where: {
-            id: req.params.id,
-          },
-        }
-      );
-      res
-        .status(200)
-        .json({ message: `Your profile has been successfully updated!` });
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  });
+  try {
+    // TODO verify that the user exists in the database.
+    const userExists = await User.findByPk(req.params.id);
 
-  // Try to find a user with the provided username in the database.
-router.get("/checkusername/:username", async (req, res) => {
-    try {
-      const user = await User.findOne({
-        where: { username: req.params.username },
+    if (!userExists) {
+      return res.status(400).json({
+        message: `The user with the provided id "${req.params.id}" does not exist. Please try again.`,
       });
-  
-      if (user) {
-        res.status(200).json(user);
-      } else {
-        res.status(200).json(false);
-      }
-    } catch (error) {
-      res.status(500).json(error);
     }
-  });
-  
-  // set up a route for users to delete their account.
-  router.delete("/delete/:id", async (req, res) => {
-    try {
-      // TODO verify that the user exists in the database.
-      const userExists = await User.findByPk(req.params.id);
-  
-      if (!userExists) {
-        return res.status(400).json({
-          message: `The user with the provided id "${req.params.id}" does not exist. Please try again.`,
-        });
-      }
-  
-      // proceed to delete the user from the database.
-      const result = await User.destroy({
+
+    // proceed to update the user in the database.
+    const result = await User.update(
+      {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        username: req.body.username,
+        about_me: req.body.about_me,
+        avatar_id: req.body.avatar_id,
+      },
+      {
         where: {
           id: req.params.id,
         },
-      });
-      res
-        .status(200)
-        .json({ message: `Your account has been successfully deleted!` });
-    } catch (error) {
-      res.status(500).json(error);
+      }
+    );
+    res
+      .status(200)
+      .json({ message: `Your profile has been successfully updated!` });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Try to find a user with the provided username in the database.
+router.get("/checkusername/:username", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { username: req.params.username },
+    });
+
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(200).json(false);
     }
-  });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Retrieve user id by username
+router.get("/getuserid/:username", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { username: req.params.username },
+    });
+
+    if (user) {
+      res.status(200).json({ userId: user.id });
+    } else {
+      res.status(200).json(false);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// set up a route for users to delete their account.
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    // TODO verify that the user exists in the database.
+    const userExists = await User.findByPk(req.params.id);
+
+    if (!userExists) {
+      return res.status(400).json({
+        message: `The user with the provided id "${req.params.id}" does not exist. Please try again.`,
+      });
+    }
+
+    // proceed to delete the user from the database.
+    const result = await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res
+      .status(200)
+      .json({ message: `Your account has been successfully deleted!` });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
--- public/handlers/user-auth.js
+++ public/handlers/user-auth.js
@@ -1,173 +1,196 @@
 const $signupBtn = $("#signup-btn");
-const $signupUsername = $('#signup-username');
-const $signupEmail = $('#signup-email');
-const $signupPassword = $('#signup-password');
+const $signupUsername = $("#signup-username");
+const $signupEmail = $("#signup-email");
+const $signupPassword = $("#signup-password");
 const $loginBtn = $("#login-btn");
 const $logoutBnt = $("#logout-btn");
 
 // invoked when the 'Create Account' button is clicked and sends the user information to the server for saving.
 async function handleSignUpClick(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const userData = {
-        username: $signupUsername.val().trim(),
-        email: $signupEmail.val().trim().toLowerCase(),
-        password: $signupPassword.val().trim(),
-    }
+  const userData = {
+    username: $signupUsername.val().trim(),
+    email: $signupEmail.val().trim().toLowerCase(),
+    password: $signupPassword.val().trim(),
+  };
 
-    const response = await fetch('/api/user/signup', {
-        method: 'POST',
-        body: JSON.stringify(userData),
-        headers: { 'Content-Type': 'application/json' },
-    });
+  const response = await fetch("/api/user/signup", {
+    method: "POST",
+    body: JSON.stringify(userData),
+    headers: { "Content-Type": "application/json" },
+  });
 
-    if (response.ok) {
-        $('.body-modal').removeAttr('cd-signin-modal--is-visible');
-        document.location.replace('/');
-        alert(`Welcome aboard, ${userData.username}! Enjoy your journey with us!`);
-    } else {
-        alert('Failed to sign up!');
-    }
+  if (response.ok) {
+    $(".body-modal").removeAttr("cd-signin-modal--is-visible");
+    document.location.replace("/");
+    alert(`Welcome aboard, ${userData.username}! Enjoy your journey with us!`);
+  } else {
+    alert("Failed to sign up!");
+  }
 }
 
 // invoked when the 'Log-in' button is clicked and sends the user information to the server for validation.
 async function handleLogInClick(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const userData = {
-        email: $('#signin-email').val().trim().toLowerCase(),
-        password: $("#signin-password").val().trim(),
-    }
+  const userData = {
+    email: $("#signin-email").val().trim().toLowerCase(),
+    password: $("#signin-password").val().trim(),
+  };
 
-    const response = await fetch('/api/user/login', {
-        method: 'POST',
-        body: JSON.stringify(userData),
-        headers: { 'Content-Type': 'application/json' },
-    });
+  const response = await fetch("/api/user/login", {
+    method: "POST",
+    body: JSON.stringify(userData),
+    headers: { "Content-Type": "application/json" },
+  });
 
-    if (response.ok) {
-        $('.body-modal').removeAttr('cd-signin-modal--is-visible');
-        document.location.replace('/');
-        alert(`You are logged in successfully!`); // to be removed. needed for testing purpose
-    } else if (response.status === 400) {
-        $("#signin-password").siblings('.error-400').addClass('cd-signin-modal__error--is-visible');
-    } else {
-        $("#signin-password").siblings('.error-500').addClass('cd-signin-modal__error--is-visible');
-    }
+  if (response.ok) {
+    const responseData = await response.json();
+    console.log("responseData", responseData);
+    const userId = responseData.userId; // Extract additional user data
+    console.log("userId", userId);
+    $(".body-modal").removeAttr("cd-signin-modal--is-visible");
+    document.location.replace(`api//pins/user/${userId}`);
+    // alert(`You are logged in successfully!`); // to be removed. needed for testing purpose
+
+    // $(".body-modal").removeAttr("cd-signin-modal--is-visible");
+    // document.location.replace(`/`);
+    // alert(`You are logged in successfully!`); // to be removed. needed for testing purpose
+  } else if (response.status === 400) {
+    $("#signin-password")
+      .siblings(".error-400")
+      .addClass("cd-signin-modal__error--is-visible");
+  } else {
+    $("#signin-password")
+      .siblings(".error-500")
+      .addClass("cd-signin-modal__error--is-visible");
+  }
 }
 
-// 
+//
 async function handleLogOutClick(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const response = await fetch('/api/user/logout', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-    });
+  const response = await fetch("/api/user/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
 
-    if (response.ok) {
-        document.location.replace('/');
-    } else if (response.status === 404) {
-        alert('User was not found');
-    } else {
-        alert('We apologize for the inconvenience. We have encountered an issue on our end, but rest assured that we are actively working to resolve it. We appreciate your understanding.')
-    }
+  if (response.ok) {
+    document.location.replace("/");
+  } else if (response.status === 404) {
+    alert("User was not found");
+  } else {
+    alert(
+      "We apologize for the inconvenience. We have encountered an issue on our end, but rest assured that we are actively working to resolve it. We appreciate your understanding."
+    );
+  }
 }
 
 // function tracks username input validation success and calls another function to display a custom error if validation fails.
 function validateSignUpUsernameField() {
-    const bool = validateUsername(); // invoke the validate function to verify if the validation has been successful.
-    
-    if (!bool) {
-        showErrorMessage($signupUsername);
-    }
+  const bool = validateUsername(); // invoke the validate function to verify if the validation has been successful.
+
+  if (!bool) {
+    showErrorMessage($signupUsername);
+  }
 }
 
 // validate that the username value is composed of 3 to 20 alphanumeric characters.
 function validateUsername() {
-    const username = $signupUsername.val().trim();
-    const alphanumericRegex = /^[a-zA-Z0-9]+$/;
-    
-    if (username.length < 3 || username.length > 20 || !alphanumericRegex.test(username)) {
-       return false;
-    } else {
-        return true;
-    }
+  const username = $signupUsername.val().trim();
+  const alphanumericRegex = /^[a-zA-Z0-9]+$/;
+
+  if (
+    username.length < 3 ||
+    username.length > 20 ||
+    !alphanumericRegex.test(username)
+  ) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 // function tracks user's email input validation success and calls another function to display a custom error if validation fails.
 function validateSignUpEmailField() {
-    const bool = validateEmail(); // true or false
-    
-    if (!bool) {
-        showErrorMessage($signupEmail);    
-    }
+  const bool = validateEmail(); // true or false
+
+  if (!bool) {
+    showErrorMessage($signupEmail);
+  }
 }
 
 // validate that the email value is in a valid email format.
 function validateEmail() {
-    const userEmail = $signupEmail.val().trim();
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const userEmail = $signupEmail.val().trim();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (!emailRegex.test(userEmail)) {
-        return false;
-    } else {
-        return true;
-    }
+  if (!emailRegex.test(userEmail)) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 // function tracks user's password input validation success and calls another function to display a custom error if validation fails.
 function validateSignUpPasswordField() {
-    const bool = validatePassword(); // true or false
+  const bool = validatePassword(); // true or false
 
-    if (!bool) {
-        showErrorMessage($signupPassword);
-    }
+  if (!bool) {
+    showErrorMessage($signupPassword);
+  }
 }
 
 // verify that the password value consists of 8 to 128 characters and includes at least one numerical digit, one special symbol, and one uppercase letter.
 function validatePassword() {
-    const userPassword = $signupPassword.val().trim();
-    const passwordRegex = /^(?=.*[0-9])(?=.*[~`!@#$%^&*()\-=_+[\]{};':"\\|,.<>/?])(?=.*[A-Z])(?=.*[a-z]).{8,128}$/;
+  const userPassword = $signupPassword.val().trim();
+  const passwordRegex =
+    /^(?=.*[0-9])(?=.*[~`!@#$%^&*()\-=_+[\]{};':"\\|,.<>/?])(?=.*[A-Z])(?=.*[a-z]).{8,128}$/;
 
-    if (!passwordRegex.test(userPassword)) {
-        return false;
-    } else {
-        return true;
-    }
+  if (!passwordRegex.test(userPassword)) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 // displayss a custom error message if any of the sign-up inputs fail to pass the validation.
 function showErrorMessage(element) {
-    element.siblings('.cd-signin-modal__error').addClass('cd-signin-modal__error--is-visible');
+  element
+    .siblings(".cd-signin-modal__error")
+    .addClass("cd-signin-modal__error--is-visible");
 
-    element.on('focus', () => {
-        element.siblings('.cd-signin-modal__error').removeClass('cd-signin-modal__error--is-visible');
-    });
-} 
+  element.on("focus", () => {
+    element
+      .siblings(".cd-signin-modal__error")
+      .removeClass("cd-signin-modal__error--is-visible");
+  });
+}
 
 // function disables or enables "create account" button based on the sign-up fields input validation
 function checkSignUpFormCompletion() {
-    const username = validateUsername();
-    const userEmail = validateEmail();  
-    const userPassword = validatePassword();
-    
-    // if all sign-up field inputs have successfully passed the validation, remove the 'disabled' attribute from the 'Create Account' button.
-    if (username && userEmail && userPassword) { 
-        $signupBtn.removeAttr('disabled');
-    } 
-    
-    // if any of the sign-up field inputs haven't passed the validation, add the 'disabled' attribute to the 'Create Account' button.
-    if (!username || !userEmail || !userPassword) {
-        $signupBtn.attr('disabled', 'disabled');
-    };
+  const username = validateUsername();
+  const userEmail = validateEmail();
+  const userPassword = validatePassword();
+
+  // if all sign-up field inputs have successfully passed the validation, remove the 'disabled' attribute from the 'Create Account' button.
+  if (username && userEmail && userPassword) {
+    $signupBtn.removeAttr("disabled");
+  }
+
+  // if any of the sign-up field inputs haven't passed the validation, add the 'disabled' attribute to the 'Create Account' button.
+  if (!username || !userEmail || !userPassword) {
+    $signupBtn.attr("disabled", "disabled");
+  }
 }
 
 // event listeners
-$loginBtn.on('click', e => handleLogInClick(e));
-$signupBtn.on('click', e => handleSignUpClick(e));
-$logoutBnt.on('click', e => handleLogOutClick(e));
-$signupUsername.on('blur', validateSignUpUsernameField);
-$signupEmail.on('blur', validateSignUpEmailField);
-$signupPassword.on('blur', validateSignUpPasswordField);
-$('.signup-input').on('keyup', checkSignUpFormCompletion); // ".signup-input" is a class assigned to all the input fields in the sign-up form.
+$loginBtn.on("click", (e) => handleLogInClick(e));
+$signupBtn.on("click", (e) => handleSignUpClick(e));
+$logoutBnt.on("click", (e) => handleLogOutClick(e));
+$signupUsername.on("blur", validateSignUpUsernameField);
+$signupEmail.on("blur", validateSignUpEmailField);
+$signupPassword.on("blur", validateSignUpPasswordField);
+$(".signup-input").on("keyup", checkSignUpFormCompletion); // ".signup-input" is a class assigned to all the input fields in the sign-up form.
