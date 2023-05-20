794ac0790b4ebb3296af5b5aa00afe06e9e7c764
JDempe <123279032+JDempe@users.noreply.github.com>
2023-05-20 11:31:15 -0400

    Fixing a merge issue

--- controllers/apiRoutes/userRoutes.js
+++ controllers/apiRoutes/userRoutes.js
@@ -24,46 +24,49 @@ router.get("/:id", async (req, res) => {
 });
 
 // set up a route for users to sign up.
-router.post('/signup', async (req, res) => {
+router.post("/signup", async (req, res) => {
   try {
-      // verify that there is no user in the database with the provided email
-      const userExists = await User.findOne({
-          where: {
-              email: req.body.email.toLowerCase().trim(),
-          }
+    // verify that there is no user in the database with the provided email
+    const userExists = await User.findOne({
+      where: {
+        email: req.body.email.toLowerCase().trim(),
+      },
+    });
+
+    if (userExists) {
+      return res.status(422).json({ errorCode: "emailExists" });
+    }
+
+    // verify that the provided username does not exist in the database.
+    const usernameExists = await User.findOne({
+      where: {
+        username: req.body.username.trim(),
+      },
+    });
+
+    if (usernameExists) {
+      return res.status(422).json({ errorCode: "usernameExists" });
+    }
+
+    // once all verification steps have been successfully completed, proceed to create the user in the database.
+    const userData = await User.create({
+      username: req.body.username.trim(),
+      email: req.body.email.toLowerCase().trim(),
+      password: req.body.password.trim(),
+    });
+
+    req.session.user_id = userData.id;
+    req.session.logged_in = 1;
+
+    req.session.save();
+
+    res
+      .status(200)
+      .json({
+        message: `Welcome aboard, ${userData.username}! Enjoy your journey with us!`,
       });
-          
-      if (userExists) {
-          return res.status(422).json({ errorCode: 'emailExists' });
-      }
-      
-      // verify that the provided username does not exist in the database.
-      const usernameExists = await User.findOne({
-          where: {
-              username: req.body.username.trim(),
-          }
-      })
-      
-      if (usernameExists) {
-          return res.status(422).json({ errorCode: 'usernameExists' }); 
-      }
-      
-      // once all verification steps have been successfully completed, proceed to create the user in the database.
-      const userData = await User.create({
-          username: req.body.username.trim(),
-          email: req.body.email.toLowerCase().trim(),
-          password: req.body.password.trim(),
-      });
-
-      req.session.user_id = userData.id;
-      req.session.logged_in = 1;
-
-      req.session.save();
-
-      res.status(200).json({ message: `Welcome aboard, ${userData.username}! Enjoy your journey with us!` });
-      
   } catch (error) {
-      res.status(500).json(error);
+    res.status(500).json(error);
   }
 });
 
@@ -118,6 +121,41 @@ router.post("/logout", async (req, res) => {
     } else {
       return res.status(404).end();
     }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// set up a route for users to update information
+router.put("/editprofile/:id", async (req, res) => {
+  try {
+    // TODO verify that the user exists in the database.
+    const userExists = await User.findByPk(req.params.id);
+
+    if (!userExists) {
+      return res.status(400).json({
+        message: `The user with the provided id "${req.params.id}" does not exist. Please try again.`,
+      });
+    }
+
+    // proceed to update the user in the database.
+    const result = await User.update(
+      {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        username: req.body.username,
+        about_me: req.body.about_me,
+        avatar_id: req.body.avatar_id,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res
+      .status(200)
+      .json({ message: `Your profile has been successfully updated!` });
   } catch (error) {
     res.status(500).json(error);
   }
@@ -204,7 +242,6 @@ router.put("/editprofile/:username", async (req, res) => {
   } catch (error) {
     res.status(500).json(error);
   }
-
 });
 
 // set up a route for users to update their own information.  Require the user to be logged in.
