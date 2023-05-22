7a1b2e920fda7de42999ea87c4f871db74706e7c
Seer <kmkevins97@gmail.com>
2023-05-21 17:54:27 -0700

    Fixed script issue, added/fixed nightmode toggle, saves to local storage

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,26 @@
-// Placeholder for when needed
+$(document).ready(function() {
+  const isNightMode = localStorage.getItem('nightMode');
+
+  if (isNightMode !== null) {
+    $('#flexSwitchCheckDefault').prop('checked', isNightMode === 'true');
+
+    if (isNightMode === 'true') {
+      $('.navbar').addClass('darkmode');
+    }
+  }
+
+  $('#flexSwitchCheckDefault').on('change', function() {
+    const isNightMode = $(this).prop('checked');
+
+    localStorage.setItem('nightMode', isNightMode);
+
+    if (isNightMode) {
+      $('.navbar').addClass('darkmode');
+    } else {
+      $('.navbar').removeClass('darkmode');
+    }
+  });
+});
 
 // Removes any empty script tags from html
 $(document).ready(function() {
@@ -9,3 +31,4 @@ $(document).ready(function() {
     });
 });
 
+console.log("script.js loaded");
