ed41dc3acc74e10840d2a4b3702912c1fe4aba35
JDempe <123279032+JDempe@users.noreply.github.com>
2023-05-22 00:49:30 -0400

    Changed navbar to have a named dropdown and fixed minor bugs on landing page

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,16 @@
-// Placeholder for when needed
-
 // Removes any empty script tags from html
-$(document).ready(function() {
-    $('script').each(function() {
-      if (!$(this).attr('src') && !$(this).text().trim()) {
-        $(this).remove();
-      }
-    });
-});
+$(document).ready(function () {
+  $("script").each(function () {
+    if (!$(this).attr("src") && !$(this).text().trim()) {
+      $(this).remove();
+    }
+  });
 
+  // use the session lookup api to check if the user is logged in
+  $.get("/api/user/session/lookup", function (data) {
+    if (data) {
+      // if the user is logged in, make the text in the logout dropdown your username
+      $("#user-username").text(`Hi, ${data.username}`);
+    }
+  });
+});
