1e13b4047ae82472809f03693fbf555a90b13d50
JDempe <123279032+JDempe@users.noreply.github.com>
2023-05-22 01:05:42 -0400

    Added avatar to navbar

--- public/js/script.js
+++ public/js/script.js
@@ -9,8 +9,12 @@ $(document).ready(function () {
   // use the session lookup api to check if the user is logged in
   $.get("/api/user/session/lookup", function (data) {
     if (data) {
-      // if the user is logged in, make the text in the logout dropdown your username
-      $("#user-username").text(`Hi, ${data.username}`);
+      // get the avatar info for the users avatar_id
+      $.get(`/api/avatars/${data.avatar_id}`, function (avatarData) {
+        // if the user is logged in, make the text in the logout dropdown your username and your avatar picture src
+        $("#user-profile-pic").attr("src", avatarData.avatarsImage);
+        $("#user-username").text(`Hi, ${data.username}`);
+      });
     }
   });
 });
